03a4943b9dccc99196c54fc519b2c5ae55ba90aa
Robert Walker <robert.walker6580@gmail.com>
2025-12-10 17:57:46 -0800

    Implement My Projects page

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='mt-24 border-t border-gray-800 py-4 text-center text-sm text-gray-400'>
+      <p>Copyright © 2025 AI Website Builder - GreatStack</p>
+    </div>
+  );
+};
+
+export default Footer;
